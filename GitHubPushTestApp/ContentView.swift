faee63f2538b2e1a13b0ea9b299ccdb932af9730
mana.u <great8life@gmail.com>
2022-06-05 22:39:04 +0900

    k

--- GitHubPushTestApp/ContentView.swift
+++ GitHubPushTestApp/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Square")
+//                .padding()
+            Square()
+//                .frame(width: 200, height: 200)
+//                .frame(minWidth: 50, idealWidth: 200, maxWidth: 400, minHeight: 50, idealHeight: 200, maxHeight: 400)
+        }
+
     }
 }
 
@@ -18,4 +24,30 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Square: Shape {
+    func path(in rect: CGRect) -> Path {
+            var path = Path()
+
+        let square: Array<CGPoint> = [
+            .init(x: rect.midX, y: rect.midY/2),
+            .init(x: rect.midX/2, y: rect.midY/2),
+            .init(x: rect.midX/2, y: rect.maxY/4*3),
+            .init(x: rect.maxX/4*3, y: rect.maxY/4*3),
+            .init(x: rect.maxX/4*3, y: rect.midY/2)
+        ]
+        path.move(to: CGPoint(x: rect.midX, y: rect.midY))
+        path.addLines(square)
+//        path.addLine(to: CGPoint(x: rect.midX/2, y: rect.midY/4))
+//        path.addLine(to: CGPoint(x: rect.midX, y: rect.midY))
+//            path.addQuadCurve(to: CGPoint(x: rect.maxX, y: rect.maxY),
+//                              control: CGPoint(x: rect.midX, y: rect.midY))
+//            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+//            path.addQuadCurve(to: CGPoint(x: rect.minX, y: rect.minY),
+//                              control: CGPoint(x: rect.maxX, y: rect.midY))
+
+
+            return path
+        }
 }
--- GitHubPushTestApp/GitHubPushTestAppApp.swift
+++ GitHubPushTestApp/GitHubPushTestAppApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct GitHubPushTestAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
+            .navigationViewStyle(.automatic)
         }
     }
 }
